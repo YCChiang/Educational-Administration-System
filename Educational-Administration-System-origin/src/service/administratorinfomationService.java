cf40eca33e80db781934bfb0b1cd250f24713ae3
2475252204@qq.com <2475252204@qq.com>
2020-05-08 09:44:18 +0800

    modify

---
+++ Educational-Administration-System-origin/src/service/administratorinfomationService.java
@@ -0,0 +1,9 @@
+package service;
+
+import entity.Student;
+import entity.Teacher;
+
+public interface administratorinfomationService {
+	public int modifyStudent(Student info);
+	public int modfiyTeacher(Teacher info);
+}
---
+++ Educational-Administration-System-origin/src/service/impl/administratorinfomationServiceimpl.java
@@ -0,0 +1,23 @@
+package service.impl;
+
+import dao.impl.studentDaoimpl;
+import dao.impl.teacherDaoimpl;
+import entity.Student;
+import entity.Teacher;
+import service.administratorinfomationService;
+
+public class administratorinfomationServiceimpl implements administratorinfomationService{
+
+	@Override
+	public int modifyStudent(Student info) {
+		studentDaoimpl DAO = new studentDaoimpl();
+		return DAO.modifyinfo(info);
+	}
+
+	@Override
+	public int modfiyTeacher(Teacher info) {
+		teacherDaoimpl DAO = new teacherDaoimpl();
+		return DAO.modifyinfo(info);
+	}
+
+}

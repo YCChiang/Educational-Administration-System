57f45ff9f90ca462f3205bf5c939a49bf64caf32
2475252204@qq.com <2475252204@qq.com>
2020-05-10 16:28:40 +0800

    modify

--- Educational-Administration-System-origin/src/pages/Administrator/teacher_select_info.java
+++ Educational-Administration-System-origin/src/pages/Administrator/teacher_select_info.java
@@ -14,6 +14,7 @@ import javax.swing.UIManager;
 
 import entity.Teacher;
 import entity.User;
+
 public class teacher_select_info extends JFrame {
 
 	private JPanel contentPane;
@@ -23,6 +24,7 @@ public class teacher_select_info extends JFrame {
 	private JTextField textField_3;
 	private JTextField textField_4;
 	private JTextField textField_5;
+
 	public teacher_select_info(Teacher info) {
 		setBackground(Color.CYAN);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -33,73 +35,76 @@ public class teacher_select_info extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		this.setTitle("教师个人信息");
-		
-		textField = new JTextField();
+
+		textField = new JTextField(info.getName());
 		textField.setBounds(95, 23, 86, 24);
 		contentPane.add(textField);
 		textField.setColumns(10);
-		
+
 		JLabel lblNewLabel = new JLabel("姓名：");
 		lblNewLabel.setFont(new Font("微软雅黑", Font.PLAIN, 15));
 		lblNewLabel.setBackground(Color.PINK);
 		lblNewLabel.setBounds(14, 26, 72, 18);
 		contentPane.add(lblNewLabel);
-		
-		textField_1 = new JTextField();
+
+		textField_1 = new JTextField(info.getId());
 		textField_1.setBounds(95, 60, 86, 24);
 		contentPane.add(textField_1);
 		textField_1.setColumns(10);
-		
+
 		JLabel lblid = new JLabel("教师ID：");
 		lblid.setFont(new Font("微软雅黑", Font.PLAIN, 15));
 		lblid.setForeground(new Color(0, 0, 0));
 		lblid.setBounds(14, 66, 72, 18);
 		contentPane.add(lblid);
-		
+
 		JLabel label = new JLabel("性别：");
 		label.setFont(new Font("微软雅黑", Font.PLAIN, 15));
 		label.setBounds(14, 102, 72, 18);
 		contentPane.add(label);
-		
-		textField_2 = new JTextField();
+		if (info.getGender() == 1) {
+			textField_2 = new JTextField("男");
+		} else {
+			textField_2 = new JTextField("女");
+		}
 		textField_2.setBounds(95, 99, 86, 24);
 		contentPane.add(textField_2);
 		textField_2.setColumns(10);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("学院：");
 		lblNewLabel_1.setFont(new Font("微软雅黑", Font.PLAIN, 15));
 		lblNewLabel_1.setBounds(14, 138, 75, 18);
 		contentPane.add(lblNewLabel_1);
-		
-		textField_3 = new JTextField();
+
+		textField_3 = new JTextField(info.getDepartment());
 		textField_3.setBounds(95, 136, 86, 24);
 		contentPane.add(textField_3);
 		textField_3.setColumns(10);
-		
+
 		JLabel label_1 = new JLabel("电话号码：");
 		label_1.setFont(new Font("微软雅黑", Font.PLAIN, 15));
 		label_1.setBounds(14, 182, 72, 18);
 		contentPane.add(label_1);
-		
-		textField_4 = new JTextField();
+
+		textField_4 = new JTextField(info.getTel());
 		textField_4.setBounds(95, 180, 86, 24);
 		contentPane.add(textField_4);
 		textField_4.setColumns(10);
-		
+
 		JLabel label_2 = new JLabel("职称：");
 		label_2.setFont(new Font("微软雅黑", Font.PLAIN, 15));
 		label_2.setBounds(14, 222, 72, 18);
 		contentPane.add(label_2);
-		
-		textField_5 = new JTextField();
+
+		textField_5 = new JTextField(info.getTitle());
 		textField_5.setBounds(95, 220, 86, 24);
 		contentPane.add(textField_5);
 		textField_5.setColumns(10);
-		
+
 		JButton button_2 = new JButton("返回");
 		button_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+
 			}
 		});
 		button_2.setFont(new Font("微软雅黑", Font.PLAIN, 15));

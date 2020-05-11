e196c8de85fc22091a0fc2167a144026e65df24c
2475252204@qq.com <2475252204@qq.com>
2020-05-11 09:46:23 +0800

    modify

--- Educational-Administration-System-origin/src/pages/TeachersPages/ClassGradeinfo.java
+++ Educational-Administration-System-origin/src/pages/TeachersPages/ClassGradeinfo.java
@@ -18,11 +18,14 @@ import service.impl.ElectiveInfoServiceImpl;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.ScrollPaneConstants;
+
 public class ClassGradeinfo extends JFrame {
 
 	private JPanel contentPane;
 	public User u;
 	public List<String> ret;
+	JButton button;
+
 	/**
 	 * Launch the application.
 	 */
@@ -31,9 +34,9 @@ public class ClassGradeinfo extends JFrame {
 	 * Create the frame.
 	 */
 	public ClassGradeinfo(User user, String cla) {
-		u=user;
+		u = user;
 		ElectiveInfoServiceImpl elect = new ElectiveInfoServiceImpl();
-		
+
 		ret = elect.findByClass_id(cla);
 		int s = ret.size();
 		setTitle("课程成绩");
@@ -43,31 +46,31 @@ public class ClassGradeinfo extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
 		scrollPane.setBounds(0, 65, 565, 254);
 		contentPane.add(scrollPane);
-		
+
 		JTextArea textArea = new JTextArea();
 		scrollPane.setViewportView(textArea);
 		textArea.setRows(s);
 		int j;
-		for(j = 0;j < s; j++)
-		{
-			textArea.append(ret.get(j)+"\r\n");
+		for (j = 0; j < s; j++) {
+			textArea.append(ret.get(j) + "\r\n");
 		}
-		
-		JButton button = new JButton("返回");
+
+		button = new JButton("返回");
 		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				new TeacherVisitGrade(user);
+				// new TeacherVisitGrade(user);
+				contentPane.setVisible(false);
 			}
 		});
 		button.setBounds(220, 332, 113, 27);
 		contentPane.add(button);
-		
+
 		JLabel label = new JLabel("课程成绩");
 		label.setBounds(238, 23, 72, 18);
 		contentPane.add(label);

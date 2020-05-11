2efb15cef92af85f1e5449d7eef81ea90e8df507
2475252204@qq.com <2475252204@qq.com>
2020-05-11 14:37:46 +0800

    modify

--- Educational-Administration-System-origin/src/service/TeacherCommentService.java
+++ Educational-Administration-System-origin/src/service/TeacherCommentService.java
@@ -6,4 +6,5 @@ import entity.Comment;
 
 public interface TeacherCommentService {
 	public List<Comment> findCommentList(String teacher_id);
+	public List<Comment> findCommentListbyteacheridandclassid(String teacher_id , String class_id);
 }
--- Educational-Administration-System-origin/src/service/impl/TeacherCommentServiceimpl.java
+++ Educational-Administration-System-origin/src/service/impl/TeacherCommentServiceimpl.java
@@ -8,9 +8,12 @@ import service.TeacherCommentService;
 
 public class TeacherCommentServiceimpl implements TeacherCommentService {
 	CommentDaoimpl DAO = new CommentDaoimpl();
+
 	@Override
 	public List<Comment> findCommentList(String teacher_id) {
 		return DAO.searchByTeacherid(teacher_id);
 	}
-	
+	public List<Comment> findCommentListbyteacheridandclassid(String teacher_id , String class_id){
+		return DAO.searchByTeacheridAndClassId(teacher_id, class_id);
+	}
 }

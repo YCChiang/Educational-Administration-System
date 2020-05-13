3241fe0b16a291a29913310a35b8289e608cbfd4
2475252204@qq.com <2475252204@qq.com>
2020-05-13 08:59:47 +0800

    modify

--- Educational-Administration-System-origin/src/pages/StudentsPages/选课new.java
+++ Educational-Administration-System-origin/src/pages/StudentsPages/选课new.java
@@ -32,22 +32,23 @@ import javax.swing.JMenu;
 import javax.swing.SwingConstants;
 import javax.swing.JCheckBoxMenuItem;
 
-public class 选课new extends JFrame{
-	public JCheckBox checkBox,chckbxNewCheckBox;
+public class 选课new extends JFrame implements ActionListener {
+	public JCheckBox checkBox, chckbxNewCheckBox;
 	public JMenu mnNewMenu;
 	private JFrame frame;
 	public User user;
 	public JTable table;
-	public JButton button_1,button_2;
-	public JCheckBoxMenuItem chckbxmntmNewCheckItem,chckbxmntmNewCheckItem_1;
+	public JButton button_1, button_2;
+	public JCheckBoxMenuItem chckbxmntmNewCheckItem, chckbxmntmNewCheckItem_1;
 	ClassInfoServiceImpl classinfoservice = null;
 	ElectiveInfoServiceImpl electiveinfoservice = null;
 	List<ClassInfo> classInfo = null;
 	private JTextField textField;
 	private JPanel contentPane;
-	public boolean IsFull,IsConflict,id,name_t;
-	//ActionListener l;
+	public boolean IsFull, IsConflict, id, name_t;
+	// ActionListener l;
 	private Massage msg;
+
 	/**
 	 * Launch the application.
 	 */
@@ -71,109 +72,99 @@ public class 选课new extends JFrame{
 		frame.setBounds(100, 100, 609, 437);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
+
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		frame.setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		textField = new JTextField();
 		textField.setBounds(169, 59, 230, 24);
 		frame.getContentPane().add(textField);
 		textField.setColumns(10);
-		
+
 		JMenuBar menuBar = new JMenuBar();
 		frame.setJMenuBar(menuBar);
-		
+
 		JMenu mnNewMenu_1 = new JMenu("请选择你要查询的方式：");
 		menuBar.add(mnNewMenu_1);
 		mnNewMenu_1.setHorizontalAlignment(SwingConstants.CENTER);
-		
+
 		JCheckBoxMenuItem chckbxmntmNewCheckItem_2 = new JCheckBoxMenuItem("课程号");
-		//boolean name_t = chckbxmntmNewCheckItem_1.isSelected();
-		chckbxmntmNewCheckItem_2.addActionListener(new ActionListener(){
+		// boolean name_t = chckbxmntmNewCheckItem_1.isSelected();
+		chckbxmntmNewCheckItem_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if( chckbxmntmNewCheckItem_2.isSelected())
-				{
+				if (chckbxmntmNewCheckItem_2.isSelected()) {
 					id = true;
 				}
 			}
 		});
 		mnNewMenu_1.add(chckbxmntmNewCheckItem_2);
-		
+
 		JCheckBoxMenuItem chckbxmntmNewCheckItem_1_1 = new JCheckBoxMenuItem("课程名称");
-		chckbxmntmNewCheckItem_1_1.addActionListener(new ActionListener(){
+		chckbxmntmNewCheckItem_1_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if( chckbxmntmNewCheckItem_1_1.isSelected())
-				{
+				if (chckbxmntmNewCheckItem_1_1.isSelected()) {
 					name_t = true;
 				}
 			}
 		});
 		mnNewMenu_1.add(chckbxmntmNewCheckItem_1_1);
-		
-		
+
 		JCheckBox checkBox = new JCheckBox("有课容量");
 		checkBox.setBounds(140, 114, 133, 27);
 		// “只显示有课容量的课程”
-	    
+
 		checkBox.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if( checkBox.isSelected())
-				{
+				if (checkBox.isSelected()) {
 					IsFull = true;
 				}
 			}
 		});
 		frame.getContentPane().add(checkBox);
-		
+
 		JCheckBox chckbxNewCheckBox = new JCheckBox("不冲突");
 		chckbxNewCheckBox.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if( chckbxNewCheckBox.isSelected())
-				{
-					IsConflict = true; 
-					//“只显示不与自己课程表冲突的课”
+				if (chckbxNewCheckBox.isSelected()) {
+					IsConflict = true;
+					// “只显示不与自己课程表冲突的课”
 				}
 			}
 		});
 		chckbxNewCheckBox.setBounds(364, 114, 217, 27);
 		frame.getContentPane().add(chckbxNewCheckBox);
-		
+
 		JButton button = new JButton("搜索");
 		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(e.getSource()== button)
-				{
+				if (e.getSource() == button) {
 					String input = textField.getText();
-					if(input == null)
-					{
-						JOptionPane.showMessageDialog(null, "请输入查询内容", "错误提示",
-								JOptionPane.ERROR_MESSAGE);
-					}
-					else {
-						//System.out.println(id);
-						//System.out.println(name_t);
-						if(id && !name_t)
-						{
-					//		System.out.println(1);
-							 classInfo = classinfoservice.seachClassById(input, "2020春", user.getname(), IsFull, IsConflict);
-					//if(classInfo == null )
-						//		System.out.println(2);
+					if (input == null) {
+						JOptionPane.showMessageDialog(null, "请输入查询内容", "错误提示", JOptionPane.ERROR_MESSAGE);
+					} else {
+						// System.out.println(id);
+						// System.out.println(name_t);
+						if (id && !name_t) {
+							// System.out.println(1);
+							classInfo = classinfoservice.seachClassById(input, "2020春", user.getname(), IsFull,
+									IsConflict);
+							// if(classInfo == null )
+							// System.out.println(2);
 
-						//	System.out.println(classInfo.isEmpty());
+							// System.out.println(classInfo.isEmpty());
+						} else if (!id && name_t) {
+							classInfo = classinfoservice.seachClassByName(input, "2020春", user.getname(), IsFull,
+									IsConflict);
 						}
-						else if(!id && name_t)
-						{
-							 classInfo = classinfoservice.seachClassByName(input, "2020春", user.getname(), IsFull, IsConflict);
-						}
-						
+
 						DefaultTableModel tableModel = (DefaultTableModel) table.getModel();
 						if (!(classInfo.isEmpty())) {
 							for (ClassInfo c : classInfo) {
 								// 将数据添加到表格中
-								tableModel.addRow(new Object[] { c.getId(), c.getName(), c.getTeacher_name(), c.getCapacity(),
-										c.getYear(), c.getStart_week() + "-" + c.getEnd_week() });
+								tableModel.addRow(new Object[] { c.getId(), c.getName(), c.getTeacher_name(),
+										c.getCapacity(), c.getYear(), c.getStart_week() + "-" + c.getEnd_week() });
 							}
 						}
 						table.setModel(tableModel);
@@ -183,56 +174,50 @@ public class 选课new extends JFrame{
 		});
 		button.setBounds(454, 58, 89, 27);
 		frame.getContentPane().add(button);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(0, 195, 600, 110);
 		contentPane.add(scrollPane);
-		
+
 		table = new JTable();
 		table.setModel(new DefaultTableModel(new Object[][] {},
 				new String[] { "\u8BFE\u7A0BID", "\u8BFE\u7A0B\u540D\u79F0", "\u6559\u5E08\u59D3\u540D",
 						"\u8BFE\u5BB9\u91CF", "\u5F00\u8BFE\u5B66\u671F", "\u5F00\u8BFE\u5468" }));
 		table.getColumnModel().getColumn(2).setMinWidth(25);
 		scrollPane.setViewportView(table);
-		
-		
-		
-		
+
 		JButton button_1 = new JButton("选课");
 		button_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(e.getSource() == button_1)
-				{
+				if (e.getSource() == button_1) {
 					try {
 						DefaultTableModel model = (DefaultTableModel) table.getModel();
 
 						int[] selectedRows = table.getSelectedRows();
 						for (int i = 0; i < selectedRows.length; i++) {
-							if(IsFull&&!IsConflict)// “只显示有课容量的课程”
+							if (IsFull && !IsConflict)// “只显示有课容量的课程”
 							{
 								ClassInfo selected = new ClassInfo();
-								String s0 = table.getValueAt(selectedRows[i],0).toString();
+								String s0 = table.getValueAt(selectedRows[i], 0).toString();
 								selected.setId(s0);
-								String s1 = table.getValueAt(selectedRows[i],1).toString();
+								String s1 = table.getValueAt(selectedRows[i], 1).toString();
 								selected.setName(s1);
-								String s2 = table.getValueAt(selectedRows[i],2).toString();
+								String s2 = table.getValueAt(selectedRows[i], 2).toString();
 								selected.setTeacher_name(s2);
-								int s3 = Integer.parseInt(table.getValueAt(selectedRows[i],3).toString());
+								int s3 = Integer.parseInt(table.getValueAt(selectedRows[i], 3).toString());
 								selected.setCapacity(s3);
-								String s4 = table.getValueAt(selectedRows[i],4).toString();
+								String s4 = table.getValueAt(selectedRows[i], 4).toString();
 								selected.setYear(s4);
-								String s5 = table.getValueAt(selectedRows[i],5).toString();
+								String s5 = table.getValueAt(selectedRows[i], 5).toString();
 								String s[] = s5.split("\\-");
 								selected.setStart_week(Integer.parseInt(s[0]));
 								selected.setEnd_week(Integer.parseInt(s[1]));
-								//	System.out.println(selected);
-								 msg = electiveinfoservice.elective(selected, user.getname());
-							}
-							else if(!IsFull&&IsConflict)
-							{
-								String selected = table.getValueAt(selectedRows[i],0).toString();
-								//	System.out.println(selected);
-								 msg = electiveinfoservice.elective(selected, user.getname());
+								// System.out.println(selected);
+								msg = electiveinfoservice.elective(selected, user.getname());
+							} else if (!IsFull && IsConflict) {
+								String selected = table.getValueAt(selectedRows[i], 0).toString();
+								// System.out.println(selected);
+								msg = electiveinfoservice.elective(selected, user.getname());
 							}
 							if (msg.isError()) {
 								// TODO 警告弹窗，消息为msg.getContent()
@@ -248,27 +233,30 @@ public class 选课new extends JFrame{
 
 						}
 						table.setModel(model);
-					}
-				  catch (Exception ex) {
+					} catch (Exception ex) {
 						ex.printStackTrace();
 						JOptionPane.showMessageDialog(null, "请选择课程", "错误提示", JOptionPane.ERROR_MESSAGE);
+					}
 				}
 			}
-			}
-		}
-		);
+		});
 		button_1.setBounds(74, 338, 113, 27);
 		contentPane.add(button_1);
-		
+
 		JButton button_2 = new JButton("返回");
-		
-		//button_2.addActionListener(l);
+
+		button_2.addActionListener(this);
 		button_2.setBounds(384, 338, 113, 27);
 		contentPane.add(button_2);
-		//frame.getContentPane().add(menu_2);
-		
-		
+		// frame.getContentPane().add(menu_2);
+
 		frame.setVisible(true);
 	}
-	
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		if (arg0.getSource() == button_2)
+		this.dispose();
+	}
+
 }
--- Educational-Administration-System-origin/src/pages/inter.java
+++ Educational-Administration-System-origin/src/pages/inter.java
@@ -2,6 +2,16 @@ package pages;
 
 import java.awt.*;
 import javax.swing.*;
+
+import org.jvnet.substance.SubstanceLookAndFeel;
+import org.jvnet.substance.border.StandardBorderPainter;
+import org.jvnet.substance.button.ClassicButtonShaper;
+import org.jvnet.substance.painter.StandardGradientPainter;
+import org.jvnet.substance.skin.SubstanceBusinessBlackSteelLookAndFeel;
+import org.jvnet.substance.theme.SubstanceEbonyTheme;
+import org.jvnet.substance.title.FlatTitlePainter;
+import org.jvnet.substance.watermark.SubstanceBinaryWatermark;
+
 import entity.User;
 import service.impl.UserServiceImpl;
 
@@ -28,7 +38,44 @@ public class inter extends JFrame implements ActionListener {
 	        //TODO exception
 	    }
 	    */
-	    
+		 /* try {  
+
+	            //设置外观  
+
+	            UIManager.setLookAndFeel(new SubstanceBusinessBlackSteelLookAndFeel());  
+
+	            JFrame.setDefaultLookAndFeelDecorated(true);  
+
+	            //设置主题   
+
+	            SubstanceLookAndFeel.setCurrentTheme(new SubstanceEbonyTheme());  
+
+	            //设置按钮外观  
+
+	            SubstanceLookAndFeel.setCurrentButtonShaper(new ClassicButtonShaper());  
+
+	            //设置水印  
+
+	            SubstanceLookAndFeel.setCurrentWatermark(new SubstanceBinaryWatermark());  
+
+	            //设置边框  
+
+	            SubstanceLookAndFeel.setCurrentBorderPainter(new StandardBorderPainter());  
+
+	            //设置渐变渲染  
+
+	            SubstanceLookAndFeel.setCurrentGradientPainter(new StandardGradientPainter());  
+
+	            //设置标题  
+
+	            SubstanceLookAndFeel.setCurrentTitlePainter(new FlatTitlePainter());  
+
+	        } catch (Exception e) {  
+
+	            System.out.println(e.getMessage());  
+
+	        }
+		  */
 		inter d1 = new inter();
 	}
 
